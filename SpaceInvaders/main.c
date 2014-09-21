fd01ee1bdfedb08eed3844d32b77755ecc019675
Ghassen Zayani <ghassenzayani@MacBook-Pro-de-Ghassen.local>
2014-09-21 02:25:54 +0200

    azerty

--- SpaceInvaders/main.c
+++ SpaceInvaders/main.c
@@ -9,10 +9,36 @@
 #include <stdio.h>
 #include <SDL2/SDL.h>
 
+#define SCREEN_WIDTH 1280
+#define SCREEN_HEIGHT 800
+
+SDL_Window *window;
+SDL_Renderer *renderer;
+void init()
+{
+    
+    if (SDL_Init(SDL_INIT_AUDIO|SDL_INIT_VIDEO|SDL_INIT_EVENTS) < 0)
+    {
+        printf("error\n");
+        exit(1);
+    }
+    window = SDL_CreateWindow("SPACE INVAIDERS", 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+    renderer = SDL_CreateRenderer(window, -1, 0);
+}
+
+void destroy()
+{
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_QUIT;
+}
+
+
 int main(int argc, const char * argv[])
 {
-
+    
     // insert code here...
-    printf("Hello, World!\n");
+    init();
+    destroy();
     return 0;
 }
