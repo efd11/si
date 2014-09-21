c93f659bc8d21fdc7c77bd168103266632ad5c92
Ghassen Zayani <ghassenzayani@MacBook-Pro-de-Ghassen.local>
2014-09-21 14:40:14 +0200

    affichage fenêtre mais sans background

--- SpaceInvaders/main.c
+++ SpaceInvaders/main.c
@@ -8,12 +8,18 @@
 
 #include <stdio.h>
 #include <SDL2/SDL.h>
+#include </Library/Frameworks/SDL2_image.framework/Headers/SDL_image.h>
 
-#define SCREEN_WIDTH 1280
+#define  SCREEN_WIDTH 1280
 #define SCREEN_HEIGHT 800
 
 SDL_Window *window;
 SDL_Renderer *renderer;
+
+SDL_Surface *sBackground;
+SDL_Texture *tBackground;
+SDL_Rect *bpos;
+
 void init()
 {
     
@@ -24,21 +30,48 @@ void init()
     }
     window = SDL_CreateWindow("SPACE INVAIDERS", 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
     renderer = SDL_CreateRenderer(window, -1, 0);
+    
+    bpos->x = 0;
+    bpos->y = 0;
+    bpos->w = SCREEN_WIDTH;
+    bpos->h = SCREEN_HEIGHT;
 }
 
 void destroy()
 {
+    SDL_DestroyTexture(tBackground);
+    SDL_FreeSurface(sBackground);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
-    SDL_QUIT;
+    SDL_Quit();
 }
 
+void load()
+{
+  
+    tBackground = IMG_LoadTexture(renderer, "background.jpg");
+}
+int i = 1;
 
 int main(int argc, const char * argv[])
 {
     
     // insert code here...
+    
+    
     init();
+    load();
+    while (i == 1) {
+        
+        //Uint32 towait;
+        //Uint32 time = SDL_GetTicks();
+        
+        SDL_RenderClear(renderer);
+        SDL_RenderCopy(renderer, tBackground, 0, bpos);
+        SDL_RenderPresent(renderer);
+        
+    }
+    
     destroy();
     return 0;
 }
